650b9fc1c2adff55a407758a204ed16fcdb0c39b
Aeon <kheireddine.ounnoughi@etu.sorbonne-universite.fr>
2025-10-17 16:08:24 +0200

    Straightness

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import heapq
 
 import matplotlib
+import osmnx.distance
+
 matplotlib.use("Agg")  # Headless-safe backend
 from functools import lru_cache
 import osmnx as ox
@@ -18,17 +20,10 @@ ox.settings.cache_folder = "cache"
 
 
 # Download graph
+"""
+Network type optipns can be found in the function's definition
+"""
 G = ox.graph_from_place("Paris, France", network_type="drive")
-
-# Convert to GeoDataFrames
-nodes, edges = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True)
-
-# Optional: save a plot of the graph
-fig, ax = ox.plot_graph(G, show=False, close=False)  # headless-friendly
-fig.savefig("output/graph.png", dpi=300)
-plt.close(fig)
-import osmnx as ox
-
 
 def djikstra_shortest_paths(G, source, weight="length"):
     """
@@ -67,11 +62,53 @@ def Closeness(G, node):
         closeness = 0
     return closeness
 
-print("------------------- Closeness Centrality ------------------")
-print("nodes in graph:", len(G.nodes))
-print("nodes : " , list(G.nodes)[0])
+
+def euclidean_distance(y1,x1, y2,x2):
+    return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def straightness_helper(G_proj, node, node2):
+    """
+    d(Euclidean) i j / d i j
+
+    IMPORTANT ---> Using djikstra to all paths here is a waste , think bout optimisiting it
+    G has to be projected
+    """
+    # Node coordinates (meters)
+    x1, y1 = G_proj.nodes[node]["x"], G_proj.nodes[node]["y"]
+    x2, y2 = G_proj.nodes[node2]["x"], G_proj.nodes[node2]["y"]
+
+    # Euclidean distance in meters
+    euclidean_m = euclidean_distance(y1,x1,y2,x2)
+
+    # Shortest path distance in meters
+    paths = djikstra_shortest_paths(G, node)
+    shortest_distance = paths[node2]
+
+    if shortest_distance == 0:
+        return 0  # avoid division by zero
+
+    print(f"distance = {shortest_distance}")
+    print(f"euclidean = {euclidean_m}")
+    straightness = euclidean_m / shortest_distance
+    return straightness
+
+
+# def straightness(G,node):
+
+
+# print("------------------- Closeness Centrality ------------------")
+# print("nodes in graph:", len(G.nodes))
+# print("nodes : " , list(G.nodes)[0])
 node_0 = list(G.nodes)[0]  # Example: first node in the graph
-node_3 = list(G.nodes)[3]
+node_3 = list(G.nodes)[150]
 print("node 0:", node_0)
-print("Node 0 neighbors , " , list(G.neighbors(node_0)))
-print("Closeness of node 0:", Closeness(G, node_0))
+# print("Node 0 neighbors , " , list(G.neighbors(node_0)))
+# print("Closeness of node 0:", Closeness(G, node_0))
+
+
+G_walk_proj = ox.project_graph(G)
+
+node_0 = list(G_walk_proj.nodes)[0]
+node_3 = list(G_walk_proj.nodes)[150]
+
+s = straightness_helper(G_walk_proj, node_0, node_3)
+print(f"Straightness from node 0 to node 3: {s:.3f}")
